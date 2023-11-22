f831955b2fbd7d58857e829e7b77aacf910d47bf
OXKuiper <o.x.kuiper@gmail.com>
2023-11-22 15:01:49 +0100

    Typo's

--- AI2.py
+++ AI2.py
@@ -8,7 +8,9 @@ Exponent is hoe vaak je vermenigvuldigd... dus gelijk aan de LENGTE van de rij!
 Voorbeeld uitvoer van meetkundige_rij(5,2,5) is [5, 10, 20, 40, 80]
 Want: Startgetal is 5, de vermenigvuldiging is steeds keer-2 (dus 5, 10, 20, etc), en de lengte vd lijst moet 5 zijn (ofwel 'de exponent')
 
-## rekenkundige_rij() ##
+Hieronder nog meer voorbeeld-uitvoer van beide functies:
+
+## rekenkundige_rij() (Dit is de 1e functie!) ##
 Voorbeeld invoer-uitvoer van wat de functie rekenkundige_rij() moet doen:
 rekenkundige_rij(1, 2, 3) geeft [1, 3, 5]
 rekenkundige_rij(3, 4, 5) geeft [3, 7, 11, 15, 19]
@@ -18,7 +20,7 @@ rekenkundige_rij(5, 3, 5) geeft [5, 8, 11, 14, 17]
 rekenkundige_rij(10, 10, 10) geeft [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
 
-## meetkundige_rij() ##
+## meetkundige_rij() (Dit is de 2e functie!) ##
 Voorbeeld invoer-uitvoer van wat de functie meetkundige_rij() moet doen:
 meetkundige_rij(1, 2, 3) geeft [1, 2, 4]
 meetkundige_rij(3, 4, 5) geeft [3, 12, 48, 192, 768]
