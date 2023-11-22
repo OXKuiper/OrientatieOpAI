bf87365dd162c5b6d7808176b4624c5c43a118b0
OXKuiper <o.x.kuiper@gmail.com>
2023-11-22 14:50:45 +0100

    Extra uitleg meetkundige_rij()

---
+++ AI2.py
@@ -0,0 +1,29 @@
+'''
+Wat extra uitleg over de 2e functie, meetkundige_rij():
+
+Start is het startgetal
+Factor is de factor, dus de verdubbelaar.
+Exponent is hoe vaak je verdubbeld... dus gelijk aan de LENGTE van de rij!
+
+Voorbeeld uitvoer van meetkundige_rij(5,2,5) is [5, 10, 20, 40, 80]
+Want: Startgetal is 5, de verdubbeling is steeds keer-2 (dus 5, 10, 20, etc), en de lengte vd lijst moet 5 zijn (ofwel 'de exponent')
+
+## rekenkundige_rij() ##
+Voorbeeld invoer-uitvoer van wat de functie rekenkundige_rij() moet doen:
+rekenkundige_rij(1, 2, 3) geeft [1, 3, 5]
+rekenkundige_rij(3, 4, 5) geeft [3, 7, 11, 15, 19]
+rekenkundige_rij(3, 5, 7) geeft [3, 8, 13, 18, 23, 28, 33]
+rekenkundige_rij(5, 2, 5) geeft [5, 7, 9, 11, 13]
+rekenkundige_rij(5, 3, 5) geeft [5, 8, 11, 14, 17]
+rekenkundige_rij(10, 10, 10) geeft [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+
+## meetkundige_rij() ##
+Voorbeeld invoer-uitvoer van wat de functie meetkundige_rij() moet doen:
+meetkundige_rij(1, 2, 3) geeft [1, 2, 4]
+meetkundige_rij(3, 4, 5) geeft [3, 12, 48, 192, 768]
+meetkundige_rij(3, 5, 7) geeft [3, 15, 75, 375, 1875, 9375, 46875]
+meetkundige_rij(5, 2, 5) geeft [5, 10, 20, 40, 80]
+meetkundige_rij(5, 3, 5) geeft [5, 15, 45, 135, 405]
+meetkundige_rij(10, 10, 10) geeft [10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000, 10000000000]
+'''
